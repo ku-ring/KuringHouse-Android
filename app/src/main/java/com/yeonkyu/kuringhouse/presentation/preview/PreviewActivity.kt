a8b41b768bacd8651873fead269cee8277b0f2be
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2022-04-08 01:42:18 +0900

    [추가] CreateRoomBottomSheet 추가

--- app/src/main/java/com/yeonkyu/kuringhouse/presentation/preview/PreviewActivity.kt
+++ app/src/main/java/com/yeonkyu/kuringhouse/presentation/preview/PreviewActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.yeonkyu.kuringhouse.R
 import com.yeonkyu.kuringhouse.databinding.ActivityPreviewBinding
+import com.yeonkyu.kuringhouse.presentation.preview.bottomsheet.CreateRoomBottomSheet
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,10 +17,18 @@ class PreviewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setupBinding()
+        setupView()
     }
 
     private fun setupBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_preview)
         binding.lifecycleOwner = this
+    }
+
+    private fun setupView() {
+        binding.previewCreateRoomBt.setOnClickListener {
+            val bottomSheet = CreateRoomBottomSheet()
+            bottomSheet.show(supportFragmentManager, bottomSheet.tag)
+        }
     }
 }
---
+++ app/src/main/java/com/yeonkyu/kuringhouse/presentation/preview/bottomsheet/CreateRoomBottomSheet.kt
@@ -0,0 +1,24 @@
+package com.yeonkyu.kuringhouse.presentation.preview.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.yeonkyu.kuringhouse.R
+
+class CreateRoomBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.AppBottomSheetDialogTheme)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottom_sheet_create_room, container, false)
+    }
+}
